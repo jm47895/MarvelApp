eccb851e4f3837a953874ea9785d02813af1169a
jm47895 <jordan.madrigal@gmail.com>
2021-11-22 22:51:01 -0600

    Add basic landscape and portrait layouts. Add view binding to main activity.

--- app/src/main/java/com/jordanmadrigal/marvelapp/ui/MainActivity.kt
+++ app/src/main/java/com/jordanmadrigal/marvelapp/ui/MainActivity.kt
@@ -2,14 +2,24 @@ package com.jordanmadrigal.marvelapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.jordanmadrigal.marvelapp.R
+import com.jordanmadrigal.marvelapp.databinding.ActivityMainBinding
+import com.jordanmadrigal.marvelapp.viewmodel.ComicViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    val viewModel: ComicViewModel by viewModels()
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
     }
 }
